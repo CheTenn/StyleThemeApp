53164fff3fae3183798bcd47f6cee615486f0db0
CheTenn <90787087+CheTenn@users.noreply.github.com>
2022-09-09 08:09:21 -0500

    Add reverse name feature

--- app/src/main/java/com/example/stylethemeap/MainActivity.java
+++ app/src/main/java/com/example/stylethemeap/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.stylethemeap;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText name;
+    TextView reversedName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        name = findViewById(R.id.nameET);
+        reversedName = findViewById(R.id.nameBackwards);
+    }
+
+    public void setReversedName(View view){
+        String reversed = "";
+        for (int i = name.getText().length() - 1; i >= 0; i--) {
+            reversed += name.getText().charAt(i);
+        }
+
+        reversedName.setText(reversed);
     }
 }
